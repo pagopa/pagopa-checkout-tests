22e01846b56ec4b78372f269c90608d7b75b2634
Simone infante <52280205+infantesimone@users.noreply.github.com>
2021-10-20 15:36:06 +0200

    Iopay 270 init (#2)
    * [IOPAY-270] init
    
    * add soak tests

--- load-tests/api/get-payment.js
+++ load-tests/api/get-payment.js
@@ -10,15 +10,15 @@ export let options = {
             preAllocatedVUs: 50,
             maxVUs: 100,
             stages: [
-              { duration: '10s', target: 100 },
-              { duration: '10s', target: 200 },
-              { duration: '10s', target: 100 },
+              { duration: '20s', target: 100 },
+              { duration: '20s', target: 200 },
+              { duration: '20s', target: 100 },
             ]
         }
     },
-    thresholds: {
-        http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
-    },
+    // thresholds: {
+    //     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    // },
 };
 
 export function setup() {
@@ -27,27 +27,25 @@ export function setup() {
 }
 
 export default function (data) {
-    // Values from env var.
-    var urlBasePath = "https://api.io.italia.it";
- 
-    var validRptId = "77777777777302000100000009424";
+    
+    const urlBasePath = "https://api.io.italia.it";
+    const validRptId = "01199250158002721253546529490";
+    
+    const headersParams = {
 
-    var headersParams = {
         headers: {
             'Content-Type': 'application/json',
         },
     };
-
-    // Send new message
-    var tag = {
+    
+    // 1. Verification Step
+    var tagVerify = {
         pagoPaMethod: "GetPaymentInfo",
     };
-    var url = `${urlBasePath}/api/payportal/v1/payment-requests/${validRptId}?recaptchaResponse=token`;
-    var r = http.get(url, headersParams, {
-        tags: tag,
+    var verifyResponse = http.get(`${urlBasePath}/api/payportal/v1/payment-requests/${validRptId}?recaptchaResponse=token`, headersParams, {
+        tags: tagVerify
     });
     
-    console.log("Get payment info " + r.status);
-    check(r, { 'status is 200': (r) => r.status === 200 }, tag);
- 
+    check(verifyResponse, { 'status is 200': (r) => r.status === 200 }, tagVerify);
+
 }
