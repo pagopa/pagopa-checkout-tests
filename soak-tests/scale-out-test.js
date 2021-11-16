492875f735b40a36f2392a7beb1eb01fb6c71bea
Marco Greco <marcoandreagreco@gmail.com>
2021-11-16 16:38:44 +0100

    [IOPAY-331] update scenario for scale-out tests

--- soak-tests/scale-out-test.js
+++ soak-tests/scale-out-test.js
@@ -4,13 +4,28 @@ import http from 'k6/http';
 * Testing the automated scaling-out 
 */
 
+/*
 export let options = {
     stages: [
-      { duration: '5m', target: 5000 }, // ramp up to 5000 users
-      { duration: '6m', target: 5000 }, // stay at 5000 for >4 min
+      { duration: '5m', target: 100 }, // ramp up to 5000 users
+      { duration: '6m', target: 100 }, // stay at 5000 for >4 min
       { duration: '1m', target: 0 }, // scale down. 
     ],
   };
+*/
+
+export let options = {
+    stages: [
+        { 
+            executor: 'constant-arrival-rate',
+            target: 70,
+            rate: 100, // 100 RPS, since timeUnit is the default 1s
+            duration: '5m',
+            preAllocatedVUs: 50,
+            maxVUs: 100,
+        }, 
+      ],
+}
 
 
 export function setup() {
@@ -21,9 +36,16 @@ export function setup() {
 export default function () {
 
     const urlBasePath = "https://api.platform.pagopa.it";
+    
+    const headersParams = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'text',
+            'User-Agent': 'agent'
+        },
+    };
 
-
-    const getInfo = http.get(`${urlBasePath}/api/checkout/payments/v1/browsers/current/info`);
+    const getInfo = http.get(`${urlBasePath}/api/checkout/payments/v1/browsers/current/info`, headersParams);
 
     if ( getInfo.status != 200 ) {
         console.log("getCurrentInfo: " + getInfo.status);
