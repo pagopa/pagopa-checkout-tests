7ea649cdb075c51333c8441d99802c74358d132b
Giovanni Berti <giovanni.berti@pagopa.it>
2022-01-13 14:21:03 +0100

    Update base URL for testing in soak tests

--- soak-tests/payments-activation.js
+++ soak-tests/payments-activation.js
@@ -4,8 +4,8 @@ import { sleep } from 'k6';
 
 export let options = {
     stages: [
-      { duration: '1m', target: 70 }, // ramp up to 70 users
-      { duration: '28m', target: 70 }, // stay at 70 for ~30 min
+      { duration: '1m', target: 5 }, // ramp up to 70 users
+      { duration: '28m', target: 5 }, // stay at 70 for ~30 min
       { duration: '1m', target: 0 }, // scale down. (optional)
     ],
   };
@@ -20,7 +20,7 @@ export default function () {
 
     const min = 302001000000000000;
     const max = 302001999999999999;
-    const urlBasePath = "https://api.io.italia.it";
+    const urlBasePath = "https://api.uat.platform.pagopa.it";
     const rptId = `77777777777${ Math.floor(Math.random() * (max - min + 1) + min)}`;
 
     const headersParams = {
@@ -33,7 +33,7 @@ export default function () {
     const tagVerify = {
         pagoPaMethod: "GetPaymentInfo",
     };
-    const verifyResponse = http.get(`${urlBasePath}/api/payportal/v1/payment-requests/${rptId}?recaptchaResponse=token`, headersParams, {
+    const verifyResponse = http.get(`${urlBasePath}/api/checkout/activations/v1/payment-requests/${rptId}?recaptchaResponse=token`, headersParams, {
         tags: tagVerify
     });
 
@@ -44,21 +44,24 @@ export default function () {
         console.log("verifyResponse: " + verifyResponse.status);
         console.log(JSON.stringify(verifyResponse.json()));
     } else {
-
         // 2. Activation Step
         const codiceContestoPagamento = verifyResponse.json().codiceContestoPagamento;
         const importoSingoloVersamento = verifyResponse.json().importoSingoloVersamento;
+
         const tagActivation = {
             pagoPaMethod: "PostActivation",
         };
-        
-        const activationResponse = http.post(
-            `${urlBasePath}/api/payportal/v1/payment-activations`,
-            JSON.stringify({ 
-            rptId, 
+
+        const body = JSON.stringify({
+            rptId,
             codiceContestoPagamento,
             importoSingoloVersamento
-            }),
+        });
+
+        const activationResponse = http.post(
+            `${urlBasePath}/api/checkout/activations/v1/payment-activations`,
+            body,
+            headersParams,
             {
             tags: tagActivation
             }
@@ -84,7 +87,7 @@ export default function () {
             let activationStatusResponse;
 
             while ( !activationCompleted && checks < maxCheck ){
-                activationStatusResponse = http.get(`${urlBasePath}/api/payportal/v1/payment-activations/${codiceContestoPagamento}`, {
+                activationStatusResponse = http.get(`${urlBasePath}/api/checkout/activations/v1/payment-activations/${codiceContestoPagamento}`, {
                     tags: tagActivationStatus,
                 });
                 checks++;
