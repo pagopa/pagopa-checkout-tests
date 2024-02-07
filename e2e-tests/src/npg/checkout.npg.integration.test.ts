474d4a5a4f2921661d948d4764278f076e06f4b8
Pietro Tota <pietro.tota@pagopa.it>
2024-02-07 16:34:36 +0100

    feat: generalize card tests

--- e2e-tests/src/npg/checkout.npg.integration.test.ts
+++ e2e-tests/src/npg/checkout.npg.integration.test.ts
@@ -39,101 +39,21 @@ describe("Checkout payment activation tests", () => {
     await page.goto(CHECKOUT_URL);
   });
   
-  it("Should correctly execute a payment with PSP Nexi", async () => {
+  it.each(CARD_TEST_DATA.cards)("Should correctly execute a payment with configuration %s", async (testData) => {
     /*
      * 1. Payment with valid notice code
     */
     const resultMessage = await payNotice(
-      generateRandomNoticeCode(CARD_TEST_DATA.nexi.fiscalCodePrefix),
+      generateRandomNoticeCode(testData.fiscalCodePrefix),
       VALID_FISCAL_CODE,
       EMAIL,
       {
-        number: String(CARD_TEST_DATA.nexi.pan),
-        expirationDate: String(CARD_TEST_DATA.nexi.expirationDate),
-        ccv: String(CARD_TEST_DATA.nexi.cvv),
+        number: String(testData.pan),
+        expirationDate: String(testData.expirationDate),
+        ccv: String(testData.cvv),
         holderName: "Test test"
       },
-      CARD_TEST_DATA.nexi.pspAbi
-    );
-
-    expect(resultMessage).toContain("Grazie, hai pagato");
-  });
-
-  it("Should correctly execute a payment with PSP Intesa", async () => {
-    /*
-     * 1. Payment with valid notice code
-    */
-    const resultMessage = await payNotice(
-      generateRandomNoticeCode(CARD_TEST_DATA.intesa.fiscalCodePrefix),
-      VALID_FISCAL_CODE,
-      EMAIL,
-      {
-        number: String(CARD_TEST_DATA.intesa.pan),
-        expirationDate: String(CARD_TEST_DATA.intesa.expirationDate),
-        ccv: String(CARD_TEST_DATA.intesa.cvv),
-        holderName: "Test test"
-      },
-      CARD_TEST_DATA.intesa.pspAbi
-    );
-
-    expect(resultMessage).toContain("Grazie, hai pagato");
-  });
-
-  it.skip("Should correctly execute a payment with PSP Unicredit", async () => {
-    /*
-     * 1. Payment with valid notice code
-    */
-    const resultMessage = await payNotice(
-      generateRandomNoticeCode(CARD_TEST_DATA.unicredit.fiscalCodePrefix),
-      VALID_FISCAL_CODE,
-      EMAIL,
-      {
-        number: String(CARD_TEST_DATA.unicredit.pan),
-        expirationDate: String(CARD_TEST_DATA.unicredit.expirationDate),
-        ccv: String(CARD_TEST_DATA.unicredit.cvv),
-        holderName: "Test test"
-      },
-      CARD_TEST_DATA.unicredit.pspAbi
-    );
-
-    expect(resultMessage).toContain("Grazie, hai pagato");
-  });
-
-  it.skip("Should correctly execute a payment with PSP Poste", async () => {
-    /*
-     * 1. Payment with valid notice code
-    */
-    const resultMessage = await payNotice(
-    generateRandomNoticeCode(CARD_TEST_DATA.poste.fiscalCodePrefix),
-    VALID_FISCAL_CODE,
-    EMAIL,
-    {
-      number: String(CARD_TEST_DATA.poste.pan),
-      expirationDate: String(CARD_TEST_DATA.poste.expirationDate),
-      ccv: String(CARD_TEST_DATA.poste.cvv),
-      holderName: "Test test"
-    },
-    CARD_TEST_DATA.poste.pspAbi
-    );
-
-    expect(resultMessage).toContain("Grazie, hai pagato");
-  });
-
-  it.skip("Should correctly execute a payment with PSP Postepay", async () => {
-    /*
-     * 1. Payment with valid notice code
-    */
-    const resultMessage = await payNotice(
-    generateRandomNoticeCode(CARD_TEST_DATA.postepay.fiscalCodePrefix),
-    VALID_FISCAL_CODE,
-    EMAIL,
-    {
-      number: String(CARD_TEST_DATA.postepay.pan),
-      expirationDate: String(CARD_TEST_DATA.postepay.expirationDate),
-      ccv: String(CARD_TEST_DATA.postepay.cvv),
-      holderName: "Test test"
-    },
-    CARD_TEST_DATA.postepay.pspAbi
+      testData.pspAbi
     );
 
     expect(resultMessage).toContain("Grazie, hai pagato");
