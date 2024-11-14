257761a09d8550c53d59af1fa1a353d42e794681
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2024-11-14 10:55:02 +0100

    add sorting psp list e2e test

--- e2e-tests/src/npg/helpers.js
+++ e2e-tests/src/npg/helpers.js
@@ -94,6 +94,44 @@ const execute_mock_authorization_npg = async () => {
   await page.waitForNavigation();
 };
 
+export const fillOnlyCardDataForm = async (cardData) => {
+  const cardNumberInput = '#frame_CARD_NUMBER';
+  const expirationDateInput = '#frame_EXPIRATION_DATE';
+  const ccvInput = '#frame_SECURITY_CODE';
+  const holderNameInput = '#frame_CARDHOLDER_NAME';
+  const continueBtnXPath = 'button[type=submit]';
+  const disabledContinueBtnXPath = 'button[type=submit][disabled=""]';
+  let iteration = 0;
+  let completed = false;
+  while (!completed) {
+    iteration++;
+    console.log(`Compiling fields...${iteration}`);
+    await page.waitForSelector(cardNumberInput, { visible: true });
+    await page.click(cardNumberInput, { clickCount: 3 });
+    await page.keyboard.type(cardData.number);
+    console.log('card number performed');
+    await page.waitForSelector(expirationDateInput, { visible: true });
+    await page.click(expirationDateInput, { clickCount: 3 });
+    await page.keyboard.type(cardData.expirationDate);
+    console.log('expiration performed');
+    await page.waitForSelector(ccvInput, { visible: true });
+    await page.click(ccvInput, { clickCount: 3 });
+    await page.keyboard.type(cardData.ccv);
+    console.log('cvv performed');
+    await page.waitForSelector(holderNameInput, { visible: true });
+    await page.click(holderNameInput, { clickCount: 3 });
+    await page.keyboard.type(cardData.holderName);
+    console.log('holder performed');
+    completed = !!!(await page.$(disabledContinueBtnXPath));
+    await page.waitForTimeout(80);
+  }
+  const continueBtn = await page.waitForSelector(continueBtnXPath, { visible: true });
+  console.log('continueBtn found');
+  await continueBtn.click();
+
+  await page.waitForNavigation();
+};
+
 export const fillCardDataForm = async (cardData, abi) => {
   const cardNumberInput = '#frame_CARD_NUMBER';
   const expirationDateInput = '#frame_EXPIRATION_DATE';
