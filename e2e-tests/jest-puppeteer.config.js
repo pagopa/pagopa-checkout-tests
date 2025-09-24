b11738772c48a287b41b64cee368c2bc90f2454a
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2025-09-24 10:28:49 +0200

    fix api tests for checkout

--- e2e-tests/jest-puppeteer.config.js
+++ e2e-tests/jest-puppeteer.config.js
@@ -3,7 +3,8 @@ module.exports = {
         dumpio: true,
         headless: "new",
         product: 'chrome',
-        args: ["--no-sandbox"] // to fix
+        args: ["--no-sandbox"], // to fix,
+        executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
     },
     browserContext: 'incognito'
 }
